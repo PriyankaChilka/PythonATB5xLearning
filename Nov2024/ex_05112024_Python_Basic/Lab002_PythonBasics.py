0189738a6b6e196a5066f977439df4bfa826cfce
Priyanka Bhaskar Nadigatla <priyanka.chilka1993@gmail.com>
2024-11-07 08:25:42 +0530

    Push 5th Nov Assignments

---
+++ Nov2024/ex_05112024_Python_Basic/Lab002_PythonBasics.py
@@ -0,0 +1,6 @@
+print(2+2,2*4,3-3,sep=",")
+print("This is my First day of Python Programming",end="! ")
+print("I am so exited")
+import keyword
+print(keyword.kwlist)
+
---
+++ Nov2024/ex_05112024_Python_Basic/Lab003_PythonBasics.py
@@ -0,0 +1,3 @@
+#Comment
+
+print("Lets focus of this Journey and enjoy to the fullest")
