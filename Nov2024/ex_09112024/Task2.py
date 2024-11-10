d1f2b869441c8b1f3652100c7c93a7c952d407c2
Priyanka Bhaskar Nadigatla <priyanka.chilka1993@gmail.com>
2024-11-10 16:15:05 +0530

    Commit 10 nov

---
+++ Nov2024/ex_09112024/Task2.py
@@ -0,0 +1,27 @@
+# Task for the Today
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+
+Dividend=int(input("Enter Dividend : "))
+Divisor=int(input("Enter Divisor : "))
+Quotient=(Dividend//Divisor)
+Remainder=(Dividend%Divisor)
+print(Dividend/Divisor)
+
+print("Quotient is : ",Quotient)
+print("Remainder is : ",Remainder)
+
+""""
+Dividend=float(input("Enter Dividend : "))
+Divisor=float(input("Enter Divisor : "))
+Quotient=(Dividend//Divisor)
+Remainder=(Dividend%Divisor)
+print(Dividend/Divisor)
+
+print("Quotient is : ",Quotient)
+print("Remainder is : ",Remainder)
+"""
