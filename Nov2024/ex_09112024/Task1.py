45a49c6d3789582f616c70f2f9f5150ff2056dd6
Priyanka Bhaskar Nadigatla <priyanka.chilka1993@gmail.com>
2024-11-10 16:03:42 +0530

    Commit 10 nov

---
+++ Nov2024/ex_09112024/Task1.py
@@ -0,0 +1,21 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
+num1=int(input("Enter number 1 : "))
+num2=int(input("Enter number 2 : "))
+num3=int(input("Enter number 3 : "))
+
+Sum=(num1+num2+num3)
+print("Addition of three numbers :",Sum)
+
+Sub=(num1-num2-num3)
+print("Subtraction of three numbers :",Sub)
+
+Mul=(num1*num2*num3)
+print("Multipication of three numbers :",Mul)
+
+Div=(num1/num2/num3)
+print("Division of three numbers :",Div)
+
+
